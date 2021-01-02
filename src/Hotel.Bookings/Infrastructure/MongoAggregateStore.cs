1bb5108e95b96585646754d26a0ea29b46b59290
Alexey Zimarev <alex@zimarev.com>
2021-01-02 18:07:41 +0100

    Fixed the version check

--- src/Hotel.Bookings/Infrastructure/MongoAggregateStore.cs
+++ src/Hotel.Bookings/Infrastructure/MongoAggregateStore.cs
@@ -12,17 +12,20 @@ namespace Hotel.Bookings.Infrastructure {
 
         public async Task Store<T, TId, TState>(T aggregate, CancellationToken cancellationToken)
             where T : Aggregate<TId, TState> where TId : AggregateId where TState : AggregateState<TId> {
+            var expectNew = aggregate.State.InitialVersionMatches(-1);
             var result = await _database.ReplaceDocument(
                 aggregate.State,
-                cfg => cfg.IsUpsert = aggregate.State.Version == -1,
+                cfg => cfg.IsUpsert = expectNew,
                 cancellationToken
             );
-            if (result.ModifiedCount == 0) throw new OptimisticConcurrencyException<TState, TId>(aggregate.State);
+            var success = expectNew ? result.UpsertedId != null : result.ModifiedCount > 0;
+            if (!success) throw new OptimisticConcurrencyException<TState, TId>(aggregate.State);
         }
 
         public async Task<T> Load<T, TId, TState>(TId id, CancellationToken cancellationToken)
             where T : Aggregate<TId, TState>, new() where TId : AggregateId where TState : AggregateState<TId> {
             var state     = await _database.LoadDocument<TState>(id.Value, cancellationToken);
+            state.SetInitialVersion();
             var aggregate = new T {State = state};
             return aggregate;
         }
